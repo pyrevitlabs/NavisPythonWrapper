fc77c4b5135ed010fd404b1e99d820d5cbbc5534
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-11-10 15:15:42 -0800

    Added symbol for ComApi

--- npw/nvswks.py
+++ npw/nvswks.py
@@ -1,7 +1,9 @@
 import clr
 
 clr.AddReference('Autodesk.Navisworks.Api')
+clr.AddReference('Autodesk.Navisworks.ComApi')
 import Autodesk.Navisworks.Api as API
+import Autodesk.Navisworks.Api.ComApi as COMAPI
 
 
 doc = API.Application.ActiveDocument
