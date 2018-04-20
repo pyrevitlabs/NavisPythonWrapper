935e01b5fd473e2d15ad3f70c133a40c4b2a12f1
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-04-20 08:16:51 -0700

    Added comapi reference

--- npw/__init__.py
+++ npw/__init__.py
@@ -3,6 +3,6 @@
 from npw.exceptions import *
 from npw.utils import logger, dotnet
 
-from npw.nvswks import API, COMAPI, doc
+from npw.nvswks import API, COMAPI, ICOMAPI, doc
 from npw import db
 from npw.ui.selection import selection
--- npw/nvswks.py
+++ npw/nvswks.py
@@ -2,8 +2,9 @@ import clr
 
 clr.AddReference('Autodesk.Navisworks.Api')
 clr.AddReference('Autodesk.Navisworks.ComApi')
+clr.AddReference('Autodesk.Navisworks.Interop.ComApi')
 import Autodesk.Navisworks.Api as API
 import Autodesk.Navisworks.Api.ComApi as COMAPI
-
+import Autodesk.Navisworks.Api.Interop.ComApi as ICOMAPI
 
 doc = API.Application.ActiveDocument
