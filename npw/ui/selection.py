9eb22b2dfe7934dc06bb30d92e4cb3afd8a74843
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-06-06 17:59:22 -0700

    Improved selection

--- npw/ui/selection.py
+++ npw/ui/selection.py
@@ -1,5 +1,12 @@
 from npw import db
-from npw import doc
+from npw import doc, API
+
+from System.Collections.Generic import List
 
 
 selection = [db.wrap(x) for x in doc.CurrentSelection.SelectedItems]
+
+
+def select_items(modelitems):
+    doc.CurrentSelection.Clear()
+    doc.CurrentSelection.AddRange(List[API.ModelItem](modelitems))
